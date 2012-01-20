016e59789cc259d86d35eb0e9e6ce2fe7cea95bf
Alex Russell <slightlyoff@chromium.org>
2012-01-20 12:53:33 -0800

    Load simple-hashtable.js, set flags to use it (or not), and pull in source versions of the original hashtable code to aid in debugging.

--- CL-loads.js
+++ CL-loads.js
@@ -17,30 +17,49 @@
     } else if (item instanceof Array) {
       return item + "";
     }
+    return item + "";
   }
 
 	// A minimal console
 	var log = function(hint, args){
     var r = "";
+    var al = args.length;
 		r += ((hint ? hint + ":" : "") + toString(args[0]));
-		for(var i = 1; i < args.length; i++){
-			r += (", " + toString(args[i]));
+		for(var i = 1; i < al; i++){
+			r += (" " + toString(args[i]));
 		}
     print(r);
 	};
 
   // Intentionally define console in the global namespace
   console = {
-    log:    function() { log(0, arguments); },
-    error:  function() { log("ERROR", arguments); },
-    warn:   function() { log("WARN", arguments); }
+    log:    function() { log(0, Array.prototype.slice.call(arguments, 0)); },
+    error:  function() { log("ERROR", Array.prototype.slice.call(arguments, 0)); },
+    warn:   function() { log("WARN", Array.prototype.slice.call(arguments, 0)); }
   };
 
 })()
 
-load('jshashtable-2.1-gjb.js');
-load('jshashset-gjb.js');
+// load('jshashtable-2.1-gjb.js');
+load('jshashtable_src.js');
+// load('jshashset-gjb.js');
+load('jshashset-pretty.js');
+
 load('CL.js')
+
+// Command-line argument processing
+if (this.arguments) {
+  this.arguments.forEach(function(a, idx) {
+    if (a.indexOf("=") != -1) {
+      var arr = a.split("=", 2);
+      if (typeof c[arr[0]] != "undefined") {
+        c[arr[0]] = JSON.parse(arr[1]);
+      }
+    }
+  });
+}
+
+load('simple-hashtable.js');
 load('ExCLError.js')
 load('ClSymbolicWeight.js')
 load('ClStrength.js')
