091ad5b490944ea69e5331d2262c3ed0c0461870
Alex Russell <slightlyoff@chromium.org>
2012-01-31 17:50:55 +0000

    Add a version of the tests that run inside a WebWorker. Refactor the unit test harness to suck less.

---
+++ WorkerTests.js
@@ -0,0 +1,67 @@
+// Copyright (C) 1998-2000 Greg J. Badros
+//
+// Use of this source code is governed by the LGPL, which can be found in the
+// COPYING.LGPL file.
+//
+// Parts Copyright (C) 2011, Alex Rusell (slightlyoff@chromium.org)
+
+"use strict";
+
+
+(function(global) {
+
+	// A minimal, batching console
+  var logTimer = null;
+  var /*it's*/logLog = [];
+  var sendLogs = function() {
+    global.postMessage(["logs", logLog]);
+    logLog.length = 0;
+  };
+
+	var log = function(hint, args){
+    if (hint) {
+      args.shfit(hit);
+    }
+    logLog.push(args);
+    clearTimeout(logTimer);
+    if (logLog.length > 200) {
+    } else {
+      logTimer = setTimeout(sendLogs, 300);
+    }
+	};
+
+  if (!global.console) {
+    // Intentionally define console in the global namespace
+    var slice = Array.prototype.slice;
+    global.console = {
+      log:    function() { log(0, slice.call(arguments, 0)); },
+      error:  function() { log("ERROR", slice.call(arguments, 0)); },
+      warn:   function() { log("WARN", slice.call(arguments, 0)); }
+    };
+  }
+
+  global.print = function(s) { console.log(s); }
+})(this);
+
+importScripts('c.js');
+importScripts('simple-hashtable.js');
+importScripts('simple-hashset.js');
+importScripts('Error.js');
+importScripts('SymbolicWeight.js');
+importScripts('Strength.js');
+importScripts('Variable.js');
+importScripts('Point.js');
+importScripts('LinearExpression.js');
+importScripts('Constraint.js');
+importScripts('LinearConstraint.js');
+importScripts('EditInfo.js');
+importScripts('Tableau.js');
+importScripts('SimplexSolver.js');
+
+importScripts('Timer.js');
+
+this.onmessage = function(m) {
+  if (m.data[0] == "init") {
+    importScripts('Tests.js');
+  }
+};
