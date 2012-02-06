41184d9cecb0846e2fc9b2833d200412d887f1cc
Alex Russell <slightlyoff@chromium.org>
2012-02-06 12:05:22 +0000

    Safari 5.x workaround. Sigh.

--- src/c.js
+++ src/c.js
@@ -7,6 +7,21 @@
 (function(scope){
 "use strict";
 
+// For Safari 5.x. Go-go-gadget ridiculously long release cycle!
+try {
+  (function(){}).bind(scope);
+} catch (e) {
+  Object.defineProperty(Function.prototype, "bind", {
+    value: function(scope) {
+      var f = this;
+      return function() { return f.apply(scope, arguments); }
+    },
+    enumerable: false,
+    configurable: true,
+    writable: true,
+  });
+}
+
 // Global
 scope.c = {
   //
