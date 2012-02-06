e7ad3191080386e8244aca49784a4d2da13f0e16
Alex Russell <slightlyoff@chromium.org>
2012-02-06 00:38:29 +0000

    Fix busted shadowing of "c".

--- src/SymbolicWeight.js
+++ src/SymbolicWeight.js
@@ -6,97 +6,103 @@
 
 (function(c) {
 
-c.SymbolicWeight = c.inherit(
-  function(w1, w2, w3) {
+c.SymbolicWeight = c.inherit({
+  initialize: function(w1, w2, w3) {
     this._values = new Array(w1, w2, w3);
   },
-  null,
-  {
-    times: function(n) {
-      return new c.SymbolicWeight(this._values[0]*n,
-                                  this._values[1]*n,
-                                  this._values[2]*n);
-    },
-    divideBy: function(n) {
-      return new c.SymbolicWeight(this._values[0]/n,
-                                  this._values[1]/n,
-                                  this._values[2]/n);
-    },
-    add: function(c) {
-      return new c.SymbolicWeight(this._values[0]+c._values[0],
-                                  this._values[1]+c._values[1],
-                                  this._values[2]+c._values[2]);
-    },
 
-    subtract: function(c) {
-      return new c.SymbolicWeight(this._values[0]-c._values[0],
-                                  this._values[1]-c._values[1],
-                                  this._values[2]-c._values[2]);
-    },
+  times: function(n) {
+    var v = this._values;
+    return new c.SymbolicWeight(v[0] * n,
+                                v[1] * n,
+                                v[2] * n);
+  },
 
-    lessThan: function(c) {
-      for (i = 0; i < this._values.length; ++i) {
-        if (this._values[i] < c._values[i]) {
-          return true;
-        } else if (this._values[i] > c._values[i]) {
-          return false;
-        }
+  divideBy: function(n) {
+    var v = this._values;
+    return new c.SymbolicWeight(v[0] / n,
+                                v[1] / n,
+                                v[2] / n);
+  },
+
+  add: function(w) {
+    var v = this._values;
+    var wv = w._values;
+    return new c.SymbolicWeight(v[0] + wv[0],
+                                v[1] + wv[1],
+                                v[2] + wv[2]);
+  },
+
+  subtract: function(w) {
+    var v = this._values;
+    var wv = w._values;
+    return new c.SymbolicWeight(v[0] - wv[0],
+                                v[1] - wv[1],
+                                v[2] - wv[2]);
+  },
+
+  lessThan: function(c) {
+    for (i = 0; i < this._values.length; ++i) {
+      if (this._values[i] < c._values[i]) {
+        return true;
+      } else if (this._values[i] > c._values[i]) {
+        return false;
       }
-      return false; // equal
-    },
-      
-    lessThanOrEqual: function(c) {
-      for (i = 0; i < this._values.length; ++i) {
-        if (this._values[i] < c._values[i]) {
-          return true;
-        } else if (this._values[i] > c._values[i]) {
-          return false;
-        }
+    }
+    return false; // equal
+  },
+    
+  lessThanOrEqual: function(c) {
+    for (i = 0; i < this._values.length; ++i) {
+      if (this._values[i] < c._values[i]) {
+        return true;
+      } else if (this._values[i] > c._values[i]) {
+        return false;
       }
-      return true; // equal
-    },
+    }
+    return true; // equal
+  },
 
-    equal: function(c) {
-      for (i = 0; i < this._values.length; ++i) {
-        if (this._values[i] != c._values[i]) {
-          return false;
-        }
+  equal: function(c) {
+    for (i = 0; i < this._values.length; ++i) {
+      if (this._values[i] != c._values[i]) {
+        return false;
       }
-      return true;
-    },
+    }
+    return true;
+  },
 
-    greaterThan: function(c) {
-      return !this.lessThanOrEqual(c);
-    },
+  greaterThan: function(c) {
+    return !this.lessThanOrEqual(c);
+  },
 
-    greaterThanOrEqual: function(c) {
-      return !this.lessThan(c);
-    },
+  greaterThanOrEqual: function(c) {
+    return !this.lessThan(c);
+  },
 
-    isNegative: function() {
-      return this.lessThan(c.SymbolicWeight.clsZero);
-    },
+  isNegative: function() {
+    return this.lessThan(c.SymbolicWeight.clsZero);
+  },
 
-    toDouble: function() {
-      sum  =  0;
-      factor = 1;
-      multiplier = 1000;
-      for (i = this._values.length - 1; i >= 0; --i) {
-        sum += this._values[i] * factor;
-        factor *= multiplier;
-      }
-      return sum;
-    },
+  toDouble: function() {
+    sum  =  0;
+    factor = 1;
+    multiplier = 1000;
+    for (i = this._values.length - 1; i >= 0; --i) {
+      sum += this._values[i] * factor;
+      factor *= multiplier;
+    }
+    return sum;
+  },
 
-    toString: function() {
-      return '[' + this._values[0] + ','
-        + this._values[1] + ','
-        + this._values[2] + ']';
-    },
+  toString: function() {
+    return '[' + this._values[0] + ','
+      + this._values[1] + ','
+      + this._values[2] + ']';
+  },
 
-    cLevels: function() { return 3; }
-  }
-);
+  cLevels: function() { return 3; }
+});
 
 c.SymbolicWeight.clsZero = new c.SymbolicWeight(0, 0, 0);
 
