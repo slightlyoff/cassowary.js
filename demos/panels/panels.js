43aa3f17165401cb688c7e56899e6526d352df16
Alex Russell <slightlyoff@chromium.org>
2012-03-11 19:45:21 +0000

    The start of a very tasty demo...a very long distance from here

---
+++ demos/panels/panels.js
@@ -0,0 +1,30 @@
+document.solver = new c.SimplexSolver();
+
+/*
+Panel = c.inherit({
+  extends: HTMLDivElement,
+  initialize: function() {
+    this.variables = {
+      width: new c.Variable(this.getId() + "_width");
+      height
+      left
+      right
+      top
+      bottom
+      contentWidth
+      contentHeight
+      contentLeft
+      contentRight
+      contentTop
+      contentBottom
+    };
+
+    // TODO(slightlyoff):
+    //      Add zero bounds constraints and sanity checking constraints
+  },
+
+  onSolved: function() {
+    this.style.... = outcome
+  }
+});
+*/
