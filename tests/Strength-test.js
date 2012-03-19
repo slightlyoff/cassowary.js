4d33bc5d7718a591ee8c97843381188707432c27
Alex Russell <slightlyoff@chromium.org>
2012-03-19 10:39:39 +0000

    Making sure I'm sane WRT how to use these things.

--- tests/Strength-test.js
+++ tests/Strength-test.js
@@ -21,8 +21,71 @@ doh.add("c.Strength", [
     t.f(c.Strength.weak.isRequired());
   },
 
-  function isSane(t) {
+  function sane(t) {
+    var s = new c.SimplexSolver();
+
+    // x = 10
+    // y = 20
+    // z = x (weak)
+    // z = y (strong)
+    // z == 20
+ 
+    var x = new c.Variable("x");
+    var y = new c.Variable("y");
+    var z = new c.Variable("z");
+
+    s.addConstraint(new c.LinearEquation(z, x, c.Strength.weak))
+     .addConstraint(new c.LinearEquation(z, y, c.Strength.strong));
+
+    s.addStay(x)
+     .addStay(y)
+     .addEditVar(x)
+     .addEditVar(y).beginEdit();
+
+    s.suggestValue(x, 10)
+     .suggestValue(y, 20).resolve();
+    s.endEdit();
+    t.t(c.approx(x.value(), 10.0));
+    t.t(c.approx(y.value(), 20.0));
+    t.t(c.approx(z.value(), 20.0));
   },
+
+  function stayEachTime(t) {
+    var s = new c.SimplexSolver();
+
+    var x = new c.Variable("x");
+    var y = new c.Variable("y");
+    var z = new c.Variable("z");
+
+    s.addConstraint(new c.LinearEquation(z, x, c.Strength.weak))
+     .addConstraint(new c.LinearEquation(z, y, c.Strength.strong));
+
+    s.addStay(x)
+     .addStay(y)
+     .addEditVar(x)
+     .addEditVar(y).beginEdit();
+
+    s.suggestValue(x, 10)
+     .suggestValue(y, 20).resolve();
+    s.endEdit();
+
+    t.t(c.approx(x.value(), 10.0));
+    t.t(c.approx(y.value(), 20.0));
+    t.t(c.approx(z.value(), 20.0));
+
+    s.addEditVar(x)
+     .addEditVar(y).beginEdit();
+
+    s.suggestValue(x, 30)
+     .suggestValue(y, 50).resolve();
+    s.endEdit();
+
+    t.t(c.approx(x.value(), 30.0));
+    t.t(c.approx(y.value(), 50.0));
+    t.t(c.approx(z.value(), 50.0));
+
+  },
+
   // FIXME(slightlyoff): MOAR TESTS
 ]);
 
