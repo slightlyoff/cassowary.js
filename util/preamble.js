5dd5daf96fcb593a5b6652fdde7b4ac33fca9dee
Alex Russell <slightlyoff@chromium.org>
2012-12-21 16:06:19 +0000

    Adding Greg to the build preamble.

--- util/preamble.js
+++ util/preamble.js
@@ -1,5 +1,6 @@
 /**
  * Parts Copyright (C) 2011-2012, Alex Russell (slightlyoff@chromium.org)
+ * Parts Copyright (C) Copyright (C) 1998-2000 Greg J. Badros
  *
  * Use of this source code is governed by the LGPL, which can be found in the
  * COPYING.LGPL file.
