3b45c571ddc0afda9a88cbe8546d5477ee177ff2
ben hockey <neonstalwart@gmail.com>
2013-02-17 14:17:09 -0600

    update built file with Apache 2.0 license

--- bin/c.js
+++ bin/c.js
@@ -2,8 +2,7 @@
  * Parts Copyright (C) 2011-2012, Alex Russell (slightlyoff@chromium.org)
  * Parts Copyright (C) Copyright (C) 1998-2000 Greg J. Badros
  *
- * Use of this source code is governed by the LGPL, which can be found in the
- * COPYING.LGPL file.
+ * Use of this source code is governed by http://www.apache.org/licenses/LICENSE-2.0
  *
  * This is a compiled version of Cassowary/JS. For source versions or to
  * contribute, see the github project:
