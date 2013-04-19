2a262e72a6cf515d07ad01c168a566fe0910a455
Alex Russell <slightlyoff@chromium.org>
2013-04-19 17:41:33 +0100

    Add start at API tests and update build.

---
+++ tests/parser/smoketest.js
@@ -0,0 +1,31 @@
+// Copyright (C) 2013, Alex Russell <slightlyoff@chromium.org>
+// Use of this source code is governed by
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+(function() {
+"use strict";
+
+var c = require("../../");
+// DOH Compat.
+var t = require("chai").assert;
+t.is = t.deepEqual;
+t.t = t;
+t.f = function(obj, str) {
+  return t.t(!obj, str);
+};
+
+describe("parser smoketests", function() {
+  it("should have a meaningful c._api", function() {
+    t.t(typeof c._api == "function");
+  })
+
+  it("can parse an empty string", function() {
+    c("");
+  })
+
+  it("returns a parse object", function() {
+    t.t(typeof c("") == "object");
+  })
+});
+
+})();
