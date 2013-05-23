c637ed3a6d88254d9921ab4036bd097db39c261a
Henri Bergius <henri.bergius@iki.fi>
2013-05-23 09:05:56 +0200

    Add a parser-based solving example

--- tests/parser/smoketest.js
+++ tests/parser/smoketest.js
@@ -28,4 +28,49 @@ describe("parser smoketests", function() {
   });
 });
 
+var chai = require('chai');
+describe('with a valid expression', function () {
+  var exp = null;
+  var values = null;
+  var solver = null;
+  it('should return a list of Constraints', function () {
+    exp = c('a + b == c');
+    chai.expect(exp).to.be.an('array');
+    chai.expect(exp[0]).to.be.instanceOf(c.Constraint);
+  });
+  it('should contain the variables', function () {
+    var terms = exp[0].expression.terms;
+    var found = 0;
+    terms.each(function (variable) {
+      found++;
+      chai.expect(variable).to.be.instanceOf(c.Variable);
+    });
+    chai.expect(found).to.equal(3);
+  });
+  it('should be convertable to a Constraint', function () {
+    solver = new c.SimplexSolver();
+    solver.addConstraint(exp[0]);
+  });
+  it('should have initial value of 0', function () {
+    exp[0].expression.terms.each(function (variable) {
+      if (variable.name === 'b') {
+        chai.expect(variable.value).to.equal(0);
+      }
+    });
+  });
+  describe('with values provided', function () {
+    it('should be solvable', function () {
+      var aVal = c('a == 5');
+      solver.addConstraint(aVal[0]);
+      var cVal = c('c == 7');
+      solver.addConstraint(cVal[0]);
+      exp[0].expression.terms.each(function (variable) {
+        if (variable.name === 'b') {
+          chai.expect(variable.value).to.equal(2);
+        }
+      });
+    });
+  });
+});
+
 })();
