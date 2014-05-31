97de1f026601415549aeb6b093d7a92fbf33c354
Alex Russell <slightlyoff@chromium.org>
2014-05-31 12:12:22 -0700

    Clobber (unused) keyCode, update perf logs, and update build.

--- perf/prof.sh
+++ perf/prof.sh
@@ -19,11 +19,10 @@ fi
 rm -rf v8.log
 rm -rf test.prof
 
-# $D8PATH --harmony --prof ../tests/run-perf.js
+$D8PATH --harmony $BENCH_FILES
+
 $D8PATH --harmony --prof --trace-opt --trace-deopt --code-comments $BENCH_FILES > deopts.log
 $D8DIR/tools/mac-tick-processor v8.log > test.prof
-
-$D8PATH --harmony $BENCH_FILES
 
 $D8PATH --trace-hydrogen --trace-phase=Z --trace-deopt --code-comments --hydrogen-track-positions --redirect-code-traces --redirect-code-traces-to=code.asm --print-opt-code $BENCH_FILES
 
