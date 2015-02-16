762d80f7f2f74e8d3b945c572497c471af40d656
Alex Russell <slightlyoff@chromium.org>
2015-02-16 11:18:13 -0800

    Clobber Safari 5.x workaround.

--- src/c.js
+++ src/c.js
@@ -8,21 +8,6 @@
 (function(scope){
 "use strict";
 
-// For Safari 5.x. Go-go-gadget ridiculously long release cycle!
-try {
-  (function(){}).bind(scope);
-} catch (e) {
-  Object.defineProperty(Function.prototype, "bind", {
-    value: function(scope) {
-      var f = this;
-      return function() { return f.apply(scope, arguments); }
-    },
-    enumerable: false,
-    configurable: true,
-    writable: true,
-  });
-}
-
 var inBrowser = (typeof scope["HTMLElement"] != "undefined");
 
 var getTagName = function(proto) {
